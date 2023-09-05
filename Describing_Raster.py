d68264b787719f65f76c6e3b6165f240d061aa70
jasondsouza22 <jasondsouza2211@gmail.com>
2023-09-05 15:12:58 +0530

    Adding All Changes

--- Describing_Raster.py
+++ Describing_Raster.py
@@ -1,3 +1,17 @@
 import arcpy
 
-raster_path = r"g"
+raster_path = r"D:\Second Year\Sem 3\Programming_for_GIS_III\P2_Describing_Data\Practical_2_ProProject\ASTGTMV003_N19E072_dem.tif"
+
+desc_obj = arcpy.Describe(raster_path)
+
+dataset_type = desc_obj.datasetType
+print(dataset_type)
+
+#The number of bands in the raster dataset
+print(desc_obj.bandCount)
+
+#Raster dataset format (Possible value are Grid, ERDAS IMAGINE, TIFF
+print(desc_obj.format)
+
+#
+print(desc_obj.pixelType)
---
+++ list_dictionaries.py
@@ -0,0 +1,4 @@
+random_list = ["Apple", "Mango", 8, True, ["a", "b"]]
+
+random_dict = {"Gaurav": 60, "Aditi": 80, "Chenoa": 100}
+
